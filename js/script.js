9107d7f1f13920748a07070d0ad618a473546fd2
laramelniker <lara.melniker@sprinklr.com>
2015-06-12 12:54:44 -0400

    added calendar and footer content

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$("a[href^=#]").click(function(e) { 
+	e.preventDefault(); 
+	var dest = $(this).attr('href'); 
+	console.log(dest); 
+	$('html,body').animate({ 
+		scrollTop: $(dest).offset().top 
+	}, 'slow'); 
+});
