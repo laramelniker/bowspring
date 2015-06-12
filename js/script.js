a50d1d4e8bb54073ee4c2323bcef285b0d127f53
laramelniker <lara.melniker@sprinklr.com>
2015-06-12 14:46:35 -0400

    smooth scrolling and finishing touches

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*SMOOTH SCROLL*/
 $("a[href^=#]").click(function(e) { 
 	e.preventDefault(); 
 	var dest = $(this).attr('href'); 
@@ -6,3 +7,15 @@ $("a[href^=#]").click(function(e) {
 		scrollTop: $(dest).offset().top 
 	}, 'slow'); 
 });
+
+/*SHOW-HIDE MOBILE MENU*/
+var hamburgerButton = document.getElementById('hamburger-button');
+hamburgerButton.addEventListener('click',function(){
+	var headerMenu = document.getElementById('nav-buttons');
+	headerMenu.classList.add('show');
+	if (headerMenu.classList[1] = 'show'){
+		headerMenu.addEventListener('click',function(){
+			headerMenu.classList.remove('show');
+		})
+	}
+}, false);
